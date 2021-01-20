7f0b81f1d4f26491451d9ced490463609f101619
Marcio Antunes <mantunes001@gmail.com>
2021-01-20 18:19:17 -0300

    Criacao do projeto

---
+++ src/test/java/com/mantunes/cursomcv2/CursomcV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mantunes.cursomcv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
