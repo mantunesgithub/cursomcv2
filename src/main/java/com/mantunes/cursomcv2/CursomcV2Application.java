6be2f531648cf5a4084d0563d894786d8720cf6e
Marcio Antunes <mantunes001@gmail.com>
2021-01-24 10:44:42 -0300

    Criando operacao de instanciacao

--- src/main/java/com/mantunes/cursomcv2/CursomcV2Application.java
+++ src/main/java/com/mantunes/cursomcv2/CursomcV2Application.java
@@ -1,13 +1,33 @@
 package com.mantunes.cursomcv2;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcV2Application {
+import com.mantunes.cursomcv2.domain.Categoria;
+import com.mantunes.cursomcv2.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcV2Application implements CommandLineRunner  {
+
+	@Autowired
+	private	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcV2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "informatica");
+		Categoria cat2 = new Categoria(null, "escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 
 }
