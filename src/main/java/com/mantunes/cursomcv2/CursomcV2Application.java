589bf37467f2f0ef9c0232a3d353fbb4f81e0680
Marcio Antunes <mantunes001@gmail.com>
2021-01-24 11:51:40 -0300

    Produto e associacao de muitos para muitos

--- src/main/java/com/mantunes/cursomcv2/CursomcV2Application.java
+++ src/main/java/com/mantunes/cursomcv2/CursomcV2Application.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mantunes.cursomcv2.domain.Categoria;
+import com.mantunes.cursomcv2.domain.Produto;
 import com.mantunes.cursomcv2.repositories.CategoriaRepository;
+import com.mantunes.cursomcv2.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcV2Application implements CommandLineRunner  {
 
 	@Autowired
 	private	CategoriaRepository categoriaRepository;
+	@Autowired
+	private	ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcV2Application.class, args);
@@ -25,9 +29,18 @@ public class CursomcV2Application implements CommandLineRunner  {
 		Categoria cat1 = new Categoria(null, "informatica");
 		Categoria cat2 = new Categoria(null, "escritorio");
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		Produto p1 = new Produto(null, "computador" , 2000.00);
+		Produto p2 = new Produto(null, "impressora" , 800.00);
+		Produto p3 = new Produto(null, "mouse" , 80.00);
 		
-		
-	}
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat1.getProdutos().addAll(Arrays.asList(p2));
 
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+	}
 }
---
+++ src/main/java/com/mantunes/cursomcv2/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.mantunes.cursomcv2.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mantunes.cursomcv2.domain.Categoria;
+import com.mantunes.cursomcv2.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
